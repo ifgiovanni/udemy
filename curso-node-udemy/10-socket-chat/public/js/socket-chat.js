fb5db433229b90ddd034e34bf1af5aeafcb97180
ifgiovanni <contactargiovanni@gmail.com>
2020-04-22 23:00:41 -0600

    Add] > Material del curso de Udemy: "Node: De cero a experto"

--- curso-node-udemy/10-socket-chat/public/js/socket-chat.js
+++ curso-node-udemy/10-socket-chat/public/js/socket-chat.js
@@ -4,47 +4,58 @@ var params = new URLSearchParams(window.location.search);
 
 if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
-    throw new Error('El nombre y sala son obligatorios');
+    throw new Error('El nombre y sala son necesarios');
 }
 
 var usuario = {
     nombre: params.get('nombre'),
     sala: params.get('sala')
-}
+};
+
+
 
 socket.on('connect', function() {
-    console.log('Connected');
-    console.log(usuario)
-    socket.emit('entrarChat', usuario, function(res) {
-        console.log('Usuarios conectados:', res);
+    console.log('Conectado al servidor');
+
+    socket.emit('entrarChat', usuario, function(resp) {
+        console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp);
     });
+
 });
 
+// escuchar
 socket.on('disconnect', function() {
-    console.log('Disconnected');
+
+    console.log('Perdimos conexión con el servidor');
+
 });
 
 
 // Enviar información
-//socket.emit('enviarMensaje', {
-//    usuario: 'Fernando',
-//    mensaje: 'Hola Mundo'
-//}, function(resp) {
-//    console.log('respuesta server: ', resp);
-//});
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-    console.log('Servidor:', mensaje);
+    //console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false);
 });
 
-// Cuando un usuario entra o sale del chat
-
-socket.on('listaPersonas', function(personas) {
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
+socket.on('listaPersona', function(personas) {
     console.log(personas);
+    renderizarUsuarios(personas);
 });
 
 // Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
-    console.log('Mensaje privado', mensaje);
+
+    console.log('Mensaje Privado:', mensaje);
+
 });
--- curso-node-udemy/10-socket-chat/server/sockets/socket.js
+++ curso-node-udemy/10-socket-chat/server/sockets/socket.js
@@ -18,20 +18,24 @@ io.on('connection', (client) => {
         client.join(data.sala);
 
         let personas = usuarios.agregarPersona(client.id, data.nombre, data.sala);
-        client.broadcast.to(data.sala).emit('listaPersonas', usuarios.getPersonasPorSala(data.sala));
+        client.broadcast.to(data.sala).emit('listaPersona', usuarios.getPersonasPorSala(data.sala));
+        client.broadcast.to(data.sala).emit('crearMensaje', crearMensaje('Administrador', `${data.nombre} se unió`));
+
         callback(usuarios.getPersonasPorSala(data.sala));
     });
 
-    client.on('crearMensaje', (data) => {
+    client.on('crearMensaje', (data, callback) => {
         let persona = usuarios.getPersona(client.id);
         let mensaje = crearMensaje(persona.nombre, data.mensaje);
         client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
+
+        callback(mensaje);
     });
 
     client.on('disconnect', () => {
         let personaBorrada = usuarios.borrarPersona(client.id);
         client.broadcast.to(personaBorrada.sala).emit('crearMensaje', crearMensaje('Administrador', `${personaBorrada.nombre} abandonó el chat`));
-        client.broadcast.to(personaBorrada.sala).emit('listaPersonas', usuarios.getPersonasPorSala(personaBorrada.sala));
+        client.broadcast.to(personaBorrada.sala).emit('listaPersona', usuarios.getPersonasPorSala(personaBorrada.sala));
 
     });
 
